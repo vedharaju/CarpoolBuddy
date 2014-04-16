5ed862a4bbcbfaad1e6c6dcbdba91ab0ff4f41dd
Vedha Sayyaparaju <vedha@mit.edu>
2014-04-16 17:57:05 -0400

    Fills in request table appropriately

--- groups.js
+++ groups.js
@@ -6,13 +6,32 @@ var params = getParams()
 
 if (params['bool'] == "true"){
 	// make the table visible
-	$(".request_table").visibility = "visible";
+	$(".request_table").css("visibility", "visible");
+
+// date from: http://stackoverflow.com/questions/1531093/how-to-get-current-date-in-javascript
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+	var yyyy = today.getFullYear();
+
+	if(dd<10) {
+	    dd='0'+dd
+	} 
+
+	if(mm<10) {
+	    mm='0'+mm
+	} 
+
+	today = mm+'/'+dd+'/'+yyyy;
+
+	var rowHtml =  "<tr>" +
+				   "<td class='bordered_cell'>" +  params['name'].replace("-", " ") + "</td>" +
+				   "<td class='bordered_cell'>" + today + "</td>" +
+				   "<td class='bordered_cell'>Pending</td>" +
+				   "<td class='unbordered_cell'><button type='button' class='btn btn-danger btn-large cancel_request_btn'>Cancel Request</button></td>" +
+				   "</tr>";
 	// adds a row to the table. the negative one means it will add it to the end of the table
-	var row  = $(".request_table").insertRow(-1);
-	row.insertCell(params['name']);
-	row.insertCell(CURRENT_TIME);
-	row.insertCell("pending");
-	row.insertCell(BUTTON);
+	$(".request_table > tbody:last").append(rowHtml);
 } 
 
 function getParams() {
